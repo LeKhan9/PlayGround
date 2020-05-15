8d35c8fa3f0924f6543e84a3eb1d2ab49cc08767
Khan, Mohammad <mkhan4@conncoll.edu>
2020-05-15 04:39:37 -0400

    Add sample backtracking algo for subsets

---
+++ algos/back-tracking/subsets.py
@@ -0,0 +1,27 @@
+def subsets(arr):
+    results = []
+    backtrack(0, [], arr, results)
+    return results
+
+def backtrack(index, curr, arr, results):
+    if index == len(arr):
+        results.append(list(curr)) # dump running subset
+        return
+
+    # explore including current val at index
+    curr.append(arr[index])
+    backtrack(index + 1, curr, arr, results)
+
+    # remove what we last added to backtrack
+    curr.pop()
+    backtrack(index + 1, curr, arr, results)
+
+
+def run_example():
+    arr = [1, 2, 3]
+
+    print(subsets(arr)) # [[1, 2, 3], [1, 2], [1, 3], [1], [2, 3], [2], [3], []]
+
+
+if __name__ == '__main__':
+    run_example()
