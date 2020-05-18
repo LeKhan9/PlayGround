e1b99c3cacfedd7fa4dd63d9bdfd3ddf0313d58d
Khan, Mohammad <mkhan4@conncoll.edu>
2020-05-18 12:14:30 -0400

    Add two bin search variants and more test cases

--- algos/hashing/two_sum.py
+++ algos/hashing/two_sum.py
@@ -2,6 +2,7 @@
     Given an array of numbers and a target number, find a pair of numbers (if any) that add up to that target.
 '''
 
+import bisect
 
 # time: O(N^2)
 # space: O(1)
@@ -70,18 +71,51 @@ def two_sum_pre_sorted(arr, target):
     return two_sum_sorting(arr, target, pre_sorted=True)
 
 
+# time: O(N * Log(N) + N * Log(N) = [2 (N * Log(N)])
+# space: O(1), assuming sort in place
+def two_sum_sorting_bin_search(arr, target, pre_sorted=False):
+    arr_size = len(arr)
+
+    if not pre_sorted:
+        arr.sort()
+
+    for i, val in enumerate(arr):
+        found_index = bisect.bisect_left(arr, target - val, i + 1, arr_size) #  ~ Log(N)
+
+        if 0 < found_index < arr_size:
+            return [val, target - val]
+
+    return []
+
+
+# time: O(N * Log(N)
+# space: O(1)
+def two_sum_pre_sorted_bin_search(arr, target):
+    return two_sum_sorting_bin_search(arr, target, pre_sorted=True)
+
+
 def run_example():
     arr = [30, 2, 5, 11, 7]
-    target = 9
 
+    target = 9
     print(two_sum_slow(arr, target)) # 1
     print(two_sum_map_fastest(arr, target)) # 2
     print(two_sum_sorting(arr, target)) # 3
+    print(two_sum_sorting_bin_search(arr, target)) # 4
 
     sorted_arr = [2, 5, 7, 11, 30]
-    target = 9
 
-    print(two_sum_pre_sorted(sorted_arr, target)) # 4
+    target = 9
+    print(two_sum_pre_sorted(sorted_arr, target)) # 5
+    print(two_sum_pre_sorted_bin_search(sorted_arr, target)) # 6
+
+    target = 88
+    print(two_sum_pre_sorted_bin_search(sorted_arr, target)) # 7
+    print(two_sum_map_fastest(arr, target)) # 8
+
+    target = 41
+    print(two_sum_map_fastest(arr, target)) # 8
+
 
     # respective outputs:
     '''
@@ -89,6 +123,11 @@ def run_example():
         2. [2, 7]
         3. [2, 7]
         4. [2, 7]
+        5. [2, 7]
+        6. [2, 7]
+        7. []
+        8. []
+        9. [11, 30]
     '''
 
 
