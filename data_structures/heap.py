7e736d3353c5fafb8dae9d1826fc6b03374a9eea
Khan, Mohammad <mkhan4@conncoll.edu>
2020-06-15 16:46:03 -0400

    Add a basic heapq wrapper class for a heap that handles numbers in max and min fashion

---
+++ data_structures/heap.py
@@ -0,0 +1,27 @@
+import heapq
+
+class NumberHeap():
+    def __init__(self, arr=None, is_max_heap=False):
+        self.arr = arr or []
+        self.is_max_heap = is_max_heap
+
+        if self.is_max_heap:
+            self.arr = [val * -1 for val in self.arr]
+
+        if self.arr:
+            heapq.heapify(self.arr)
+
+
+    def push(self, itm):
+        if self.is_max_heap:
+            itm = itm * -1
+
+        heapq.heappush(self.arr, itm)
+
+    def pop(self ):
+        itm = heapq.heappop(self.arr)
+
+        if self.is_max_heap:
+            itm = -1 * itm
+
+        return itm
